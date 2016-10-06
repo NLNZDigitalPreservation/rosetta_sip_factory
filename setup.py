fad2749e45ae284fc57ccaccbbbf05d1cbbcef87
Sean Mosely <sean.mosely@gmail.com>
2016-10-07 11:03:20 +1300

    more minor changes to setup.py

--- setup.py
+++ setup.py
@@ -11,10 +11,10 @@ config = {
 	'author':'Sean Mosely',
 	'author_email':'sean.mosely@gmail.com',
 	'packages':['rosetta_sip_factory',],
-	'description':'Python library for building Submission Information Packages for the Rosetta digital preservation application',
+	'description':'Python library for building Submission Information Packages for the Ex Libris Rosetta digital preservation application',
 	'install_requires':['lxml==3.6.4',],
 	'download_url': 'https://github.com/NLNZDigitalPreservation/rosetta_sip_factory/archive/v'+VERSION+'.tar.gz',
-	'license': 'MIT'
+	'license': 'MIT',
 	}
 
 setup(**config)
